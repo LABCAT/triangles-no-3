d625a6f81b8b3b5b1eca7b45ecbc32d6b794eb44
LABCAT <shane.watters.nz@gmail.com>
2021-04-09 18:25:27 +1200

    Audio integration and sketch experimentation.

--- src/js/P5Sketch.js
+++ src/js/P5Sketch.js
@@ -1,6 +1,10 @@
 import React, { useRef, useEffect } from "react";
+import "./globals";
+import "p5/lib/addons/p5.sound";
 import * as p5 from "p5";
 import ShuffleArray from "./ShuffleArray.js";
+import audio from "../audio/triangles-no-3.ogg";
+import cueSet1 from "./cueSet1.js";
 
 const P5Sketch = () => {
     const sketchRef = useRef();
@@ -12,6 +16,8 @@ const P5Sketch = () => {
         p.canvasWidth = window.innerWidth;
 
         p.canvasHeight = window.innerHeight;
+
+        p.song = null;
 
         p.shapeSize = 0;
 
@@ -28,21 +34,35 @@ const P5Sketch = () => {
 
         p.colours = [];
 
+        p.cueSet1Completed = [];
+
         p.setup = () => {
+            p.song = p.loadSound(audio);
             p.canvas = p.createCanvas(p.canvasWidth, p.canvasHeight);
             p.noFill();
-            p.noFill();
+            p.noLoop();
             p.strokeJoin(p.ROUND);
             p.strokeCap(p.ROUND);
             p.populateTrianglesArray();    
-            p.populateColoursArray();    
+            p.populateColoursArray();  
+             
+            p.song.onended(p.logCredits);
+
+            for (let i = 0; i < cueSet1.length; i++) {
+              let vars = {
+                currentCue: i + 1,
+                duration: cueSet1[i].duration,
+                durationTicks: cueSet1[i].durationTicks,
+              };
+              p.song.addCue(cueSet1[i].time, p.executeCueSet1, vars);
+            }
         };
 
         p.populateTrianglesArray = () => {
             const sizeLimit = p.height / 8;
             const limitX = p.width + sizeLimit;
             const limitY = p.height + sizeLimit;
-            let x = 0 - sizeLimit;
+            let x = 0 - sizeLimit * 4;
             while(x < limitX){
                 let y = 0 - sizeLimit;
                 x = x + sizeLimit;
@@ -78,21 +98,33 @@ const P5Sketch = () => {
             }
         };
 
-        p.colourSwitch = true;
-
         p.draw = () => {
-            const sizeLimit = p.height / 8;
-            p.strokeWeight(sizeLimit/16);
-            p.background(255);
-            p.drawTriangleGrid(p.triangles, sizeLimit, p.shapeSize);
-            p.shapeSize++
-            p.shapeSize =
-              p.shapeSize < sizeLimit * 1 ? p.shapeSize : 0;
+            
+        };
 
-            if(p.shapeSize < 1){
-                p.populateColoursArray(p.colourSwitch);
-                p.colourSwitch = !p.colourSwitch;
+        p.colourSwitch = false;
+
+        p.executeCueSet1 = (vars) => {
+          if (!p.cueSet1Completed.includes(vars.currentCue)) {
+            p.cueSet1Completed.push(vars.currentCue);
+            let sizeLimit = p.height / 8;
+            p.strokeWeight(sizeLimit / 16);
+            //sizeLimit = p.colourSwitch ? sizeLimit - sizeLimit / 16 : sizeLimit;
+            p.clear();
+            p.background(255);
+            let shapeSize = 0;
+            const delayAmount = parseInt(vars.duration * 1000) / sizeLimit;
+            for(let i = 0; i < sizeLimit; i++){
+              setTimeout(
+                  function () {
+                      p.drawTriangleGrid(p.triangles, sizeLimit * 4, i);
+                  },
+                  (delayAmount * i)
+              );
             }
+            p.populateColoursArray(p.colourSwitch);
+            p.colourSwitch = !p.colourSwitch;
+          }
         };
 
         p.drawTriangleGrid = (triangles, sizeLimit, size) => {
@@ -105,11 +137,16 @@ const P5Sketch = () => {
                 p.fill(colours.colour1);
                 p.stroke(colours.stroke1);
                 p.equilateral(triangle.x, triangle.y, size);
+                
+            }
+            for(let i = 0; i < triangles.length; i++){
+                triangle = triangles[i];
+                colours = p.colours[i];
                 p.fill(colours.colour2);
                 p.stroke(colours.stroke2);
                 p.equilateral(
                   triangle.x + sizeLimit / 2,
-                  triangle.y,
+                  triangle.y + sizeLimit / 8,
                   size,
                   true
                 );
@@ -127,6 +164,43 @@ const P5Sketch = () => {
             const x3 = x + width;
             const y3 = y;
             p.triangle(x1, y1, x2, y2, x3, y3);
+        };
+
+        p.mousePressed = () => {
+          if (p.song.isPlaying()) {
+            p.song.pause();
+          } else {
+            if (
+              parseInt(p.song.currentTime()) >= parseInt(p.song.buffer.duration)
+            ) {
+              p.reset();
+            }
+            //document.getElementById("play-icon").classList.add("fade-out");
+            p.canvas.addClass("fade-in");
+            p.song.play();
+          }
+        };
+
+        p.creditsLogged = false;
+
+        p.logCredits = () => {
+          if (
+            !p.creditsLogged &&
+            parseInt(p.song.currentTime()) >= parseInt(p.song.buffer.duration)
+          ) {
+            p.creditsLogged = true;
+            console.log(
+              "Music By: http://labcat.nz/",
+              "\n",
+              "Animation By: https://github.com/LABCAT/triangles-no-3"
+            );
+            p.song.stop();
+          }
+        };
+
+        p.reset = () => {
+          p.clear();
+          p.cueSet1Completed = [];
         };
 
         p.updateCanvasDimensions = () => {
